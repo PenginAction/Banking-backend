edf12cf55e6aed5dcfea2c13e29d251f98e8f592
PenginAction <udonfox15@gmail.com>
2023-06-12 15:11:45 +0900

    設定ファイル追加

---
+++ config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/go-ini/ini"
+)
+
+type Configs struct {
+	DbUser      string
+	DbPass      string
+	DbLocal     string
+	DbPort      string
+	DbName      string
+	BankLogfile string
+	Port        string
+}
+
+var Config Configs
+
+func init() {
+	cfg, err := ini.Load("config.ini")
+	if err != nil {
+		log.Printf("Failed to read file: %v", err)
+		os.Exit(1)
+	}
+
+	Config = Configs{
+		DbUser:  cfg.Section("database").Key("dbuser").String(),
+		DbPass:  cfg.Section("database").Key("dbpass").String(),
+		DbLocal: cfg.Section("database").Key("dblocal").String(),
+		DbPort:  cfg.Section("database").Key("dbport").String(),
+		DbName:  cfg.Section("database").Key("dbname").String(),
+	}
+}
