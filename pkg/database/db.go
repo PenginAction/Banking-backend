c3085069e965f6b387e953e301d6e9e174488c4a
PenginAction <udonfox15@gmail.com>
2023-06-13 20:47:23 +0900

    databaseパッケージを追加.

---
+++ pkg/database/db.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"bank/config"
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var Db *sql.DB
+
+func init(){
+	cmd := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", 
+		config.Config.DbUser,
+		config.Config.DbPass,
+		config.Config.DbLocal,
+		config.Config.DbPort,
+		config.Config.DbName,
+	)
+
+	var  err error
+	Db, err = sql.Open("mysql",cmd)
+	if err != nil{
+		log.Fatal("err")
+	}
+}
