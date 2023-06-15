bc09b6e6cd3e026e1dbdfa8c4c2ddee7f63f684e
PenginAction <udonfox15@gmail.com>
2023-06-15 17:03:51 +0900

    ログイン・ユーザー認証機能を追加

--- app/webserver/webserver.go
+++ app/webserver/webserver.go
@@ -3,11 +3,16 @@ package webserver
 import (
 	"bank/app/userData"
 	"bank/config"
+	"bank/utils"
 	"encoding/json"
 	"fmt"
 	"html/template"
 	"net/http"
+
+	"github.com/gorilla/sessions"
 )
+
+var store = sessions.NewCookieStore([]byte("secret-key"))
 
 
 func CreateAccountHandler(w http.ResponseWriter, r *http.Request){
@@ -28,7 +33,7 @@ func CreateAccountHandler(w http.ResponseWriter, r *http.Request){
 	w.WriteHeader(http.StatusOK)
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request){
+func firstHandler(w http.ResponseWriter, r *http.Request){
 	tmpl, err := template.ParseFiles("app/templates/create.html")
 	if err != nil{
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -40,8 +45,90 @@ func homeHandler(w http.ResponseWriter, r *http.Request){
 	}
 }
 
+func LoginHandler(w http.ResponseWriter, r *http.Request){
+	if r.Method == "POST" {
+		email := r.FormValue("email")
+		password := r.FormValue("password")
+
+		user, err := userdata.GetAccountByEmail(email)
+		if err != nil{
+			http.Error(w, "メールアドレスかパスワードが一致しません", http.StatusUnauthorized)
+			return
+		}
+
+		err = userdata.CompareHashAndPassword(user.Password, password)
+		if err != nil{
+			http.Error(w, "メールアドレスかパスワードが一致しません", http.StatusUnauthorized)
+			return
+		}
+
+		session, err := store.Get(r, "session-name")
+		if err != nil{
+			http.Error(w, "Sessionの取得に失敗しました", http.StatusInternalServerError)
+		}
+		session.Values["authenticated"] = true
+		session.Values["user_id"] = user.UserId
+		err = session.Save(r, w)
+		if err != nil {
+			http.Error(w, "Sessionの取得に失敗しました", http.StatusInternalServerError)
+			return
+		}
+
+		http.Redirect(w, r, "/account", http.StatusSeeOther)
+	} else {
+		// tmpl, err := template.ParseFiles("app/templates/login.html")
+		err := utils.RenderTemplate(w, "app/templates/login.html", nil)
+		if err != nil{
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func AuthRequiredHandler(handler http.HandlerFunc) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		session, err := store.Get(r, "session-name")
+		if err != nil {
+			http.Error(w, "Sessionの取得に失敗しました", http.StatusInternalServerError)
+			return
+		}
+
+		if auth, ok := session.Values["authenticated"].(bool); !ok || !auth{
+			http.Error(w, "/login", http.StatusSeeOther)
+			return
+		}
+		handler(w, r)
+	}
+}
+
+func AccountHandler(w http.ResponseWriter, r *http.Request){
+    session, err := store.Get(r, "session-name")
+	if err != nil {
+		http.Error(w, "Sessionの取得に失敗しました", http.StatusInternalServerError)
+		return
+	}
+    userID, ok := session.Values["user_id"].(int)
+	if !ok {
+		http.Error(w, "Session内のユーザーIDの取得に失敗しました", http.StatusInternalServerError)
+	}
+
+    user, err := userdata.GetAccountById(userID)
+    if err != nil {
+        http.Error(w, "ユーザー情報の取得に失敗しました", http.StatusInternalServerError)
+        return
+    }
+	err = utils.RenderTemplate(w, "app/templates/ballanceAcount.html", user)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+}
+
 func Start() error{
 	http.HandleFunc("/create", CreateAccountHandler)
-	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/", firstHandler)
+	http.HandleFunc("/login", LoginHandler)
+	http.HandleFunc("/account", AuthRequiredHandler(AccountHandler))
 	return http.ListenAndServe(fmt.Sprintf(":%d", config.Config.Port), nil)
 }
